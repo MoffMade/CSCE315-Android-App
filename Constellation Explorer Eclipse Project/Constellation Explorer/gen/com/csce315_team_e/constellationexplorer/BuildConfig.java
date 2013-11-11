48ef337378beb749aa1e4589ff270b6a70b9b251
MoffMade <cory.maughmer@neo.tamu.edu>
2013-11-11 14:01:48 -0600

    Built GUI for Main menu, Help, Load Game
    Also updated file structure to facilitate import into the ADT Eclipse
    workspace.

---
+++ Constellation Explorer Eclipse Project/Constellation Explorer/gen/com/csce315_team_e/constellationexplorer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.csce315_team_e.constellationexplorer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- Constellation Explorer/gen/com/CECE315_TEAM_E/constellationexplorer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.CECE315_TEAM_E.constellationexplorer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
